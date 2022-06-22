85c083225c6fae54abcfe9605f55f624322d6d7f
FlutLab.io <>
2022-06-22 15:21:33 +0000

    Autosave

--- lib/try_retrieve.dart
+++ lib/try_retrieve.dart
@@ -19,27 +19,31 @@ class _SecondPageState extends State<SecondPage> {
   final FirebaseFirestore fb = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      child: FutureBuilder(
-        future: getImages(),
-        builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    contentPadding: EdgeInsets.all(8.0),
-                    title: Text(snapshot.data!.docs[index].data()["name"]),
-                    leading: Image.network(snapshot.data!.docs[index].data()["url"], fit: BoxFit.fill),
-                  );
-                });
-          } else if (snapshot.connectionState == ConnectionState.none) {
-            return Text("No data");
-          }
-          return CircularProgressIndicator();
-        },
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: FutureBuilder(
+            future: getImages(),
+            builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        contentPadding: EdgeInsets.all(8.0),
+                        title: Text(snapshot.data!.docs[index].data()["name"]),
+                        leading: Image.network(snapshot.data!.docs[index].data()["url"], fit: BoxFit.fill),
+                      );
+                    });
+              } else if (snapshot.connectionState == ConnectionState.none) {
+                return Text("No data");
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
