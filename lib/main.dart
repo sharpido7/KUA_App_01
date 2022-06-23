6daa2493712654280ebe62cc7d75dfd4a3988154
FlutLab.io <>
2022-06-23 14:09:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:kua_app_01/upload_image.dart';
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(ImageUpload());
+  runApp(ProfileScreen());
 }
 
 class MyApp extends StatelessWidget {
--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -1,142 +1,105 @@
-import 'dart:io';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
 
+// Future<Widget> _getImage(BuildContext context, String imageName) async{
+//   Image image;
+//   await FireStorageService.loadImage(context, imageName).then((value)
+//   {
+//     image = Image.network(
+//     value.toString(),
+//     fit: BoxFit.scaleDown,
+//     );
+
+//   });
+
+//   return image;
+
+// }
+
+// class FireStorageService extends ChangeNotifier{
+//   FireStorageService();
+//   static Future<dynamic> loadImage(BuildContext context, String Image) async{
+//     return await FirebaseStorage.instance.ref().child(Image).getDownloadURL();
+//   }
+// }
+//
+//
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
+import 'dart:io';
 
-//void main() => runApp(MyApp());
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
-class ImageUpload extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Storage Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: UploadingImageToFirebaseStorage(),
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+  Color primary = const Color(0xffeef444c);
+  String profilePicLink = "";
+
+  void pickUploadProfilePic() async {
+    final image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 512,
+      maxWidth: 512,
+      imageQuality: 90,
     );
-  }
-}
 
-final Color yellow = Color(0xfffbc31b);
-final Color orange = Color(0xfffb6900);
+    Reference ref = FirebaseStorage.instance.ref().child("profilepic.jpg");
 
-class UploadingImageToFirebaseStorage extends StatefulWidget {
-  @override
-  _UploadingImageToFirebaseStorageState createState() => _UploadingImageToFirebaseStorageState();
-}
+    await ref.putFile(File(image!.path));
 
-class _UploadingImageToFirebaseStorageState extends State<UploadingImageToFirebaseStorage> {
-  File _imageFile;
-
-  ///NOTE: Only supported on Android & iOS
-  ///Needs image_picker plugin {https://pub.dev/packages/image_picker}
-  final picker = ImagePicker();
-
-  Future pickImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
-
-    setState(() {
-      _imageFile = File(pickedFile.path);
+    ref.getDownloadURL().then((value) async {
+      setState(() {
+        profilePicLink = value;
+      });
     });
   }
 
-  Future uploadImageToFirebase(BuildContext context) async {
-    String fileName = basename(_imageFile.path);
-    StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child('uploads/$fileName');
-    StorageUploadTask uploadTask = firebaseStorageRef.putFile(_imageFile);
-    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
-    taskSnapshot.ref.getDownloadURL().then(
-          (value) => print("Done: $value"),
-        );
-  }
-
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: 360,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
-                gradient: LinearGradient(colors: [
-                  orange,
-                  yellow
-                ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 80),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Text(
-                      "Uploading Image to Firebase Storage",
-                      style: TextStyle(color: Colors.white, fontSize: 28, fontStyle: FontStyle.italic),
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                pickUploadProfilePic();
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 80, bottom: 24),
+                height: 120,
+                width: 120,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: primary,
                 ),
-                SizedBox(height: 20.0),
-                Expanded(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        height: double.infinity,
-                        margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(30.0),
-                          child: _imageFile != null
-                              ? Image.file(_imageFile)
-                              : FlatButton(
-                                  child: Icon(
-                                    Icons.add_a_photo,
-                                    size: 50,
-                                  ),
-                                  onPressed: pickImage,
-                                ),
+                child: Center(
+                  child: profilePicLink == " "
+                      ? const Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 80,
+                        )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(profilePicLink),
                         ),
-                      ),
-                    ],
-                  ),
                 ),
-                uploadImageButton(context),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget uploadImageButton(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-            margin: const EdgeInsets.only(top: 30, left: 20.0, right: 20.0, bottom: 20.0),
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    yellow,
-                    orange
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(30.0)),
-            child: FlatButton(
-              onPressed: () => uploadImageToFirebase(context),
-              child: Text(
-                "Upload Image",
-                style: TextStyle(fontSize: 20),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
