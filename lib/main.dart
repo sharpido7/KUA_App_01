07bd58dc8507338c7464bc5aaa16c88251a1132f
FlutLab.io <>
2022-06-24 07:27:33 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kua_app_01/julow_image_up.dart';
 import 'package:kua_app_01/registration.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:kua_app_01/new_homepage.dart';
@@ -10,7 +11,7 @@ import 'package:kua_app_01/upload_image.dart';
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(AddImage());
+  runApp(Up());
 }
 
 class MyApp extends StatelessWidget {
